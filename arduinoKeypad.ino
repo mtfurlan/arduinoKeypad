801433a098e428536970d57a94bcec9709dc2f71
Mike Williams <mswillia@mtu.edu>
2014-03-26 21:17:59 -0400

    A slightly better yet much worse arduino keypad.
    All buttons checked via loops now.
    Function pointers in arrays for calling button functions (Yeah, I went there)

--- arduinoKeypad.ino
+++ arduinoKeypad.ino
@@ -1,24 +1,64 @@
-const int DEBOUNCE = 50;
+const int button_pins[] = {2,3,4,5,6,7};
+int button_state[] = {1,1,1,1,1,1};
+int button_state_last[] = {1,1,1,1,1,1};
+int debounce[] = {0,0,0,0,0,0};
+const int debounce_time = 50;
 
-const int GREEN_PIN = 2;
-int greenState = 1;
-int greenStateLast = 1;
-int greenDebounce = 0;
+void enter();
+void science();
+void bitches();
+void del();
+void bell();
+void what();
 
-void setup(){
-  Serial.begin(9600);
-  pinMode(GREEN_PIN, INPUT_PULLUP);
-  
+void (*button_functions[])() = {&enter, &science, &bitches, &del, &bell, &what};
+
+void enter() {
+  Keyboard.write(10);
 }
 
-void loop(){
-  greenState = digitalRead(GREEN_PIN);
-  if(millis() - greenDebounce > DEBOUNCE && greenState != greenStateLast){
-    if(greenState == LOW){
-      Serial.println("science");
-    }
-    greenStateLast = greenState;
-    greenDebounce = millis();
+void science() {
+      Keyboard.press(131);
+      Keyboard.press('r');
+      delay(10);
+      Keyboard.releaseAll();
+      delay(100);
+      Keyboard.println("firefox http://xkcd.com/683/");
+}
+
+void bitches() {
+  Keyboard.println("Yeah, Science Bitches!");
+}
+
+void del() {
+  Keyboard.write(127);
+}
+
+void bell() {
+  Keyboard.write(7);
+}
+
+void what() {
+  Keyboard.write(0);
+}
+
+void setup() {
+  int i = 0;
+  for (; i < 6; ++i) {
+    pinMode(button_pins[i], INPUT_PULLUP);
   }
 }
 
+void loop() {
+  int i = 0;
+  for (; i < 6; ++i) {
+    button_state[i] = digitalRead(button_pins[i]);
+    if (millis() - debounce[i] > debounce_time && button_state[i] != button_state_last[i]) {
+      if (button_state[i] == LOW) {
+        button_functions[i]();
+      }
+      button_state_last[i] = button_state[i];
+      debounce[i] = millis();
+    }
+  }
+}
