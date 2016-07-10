b46f0df23ea7434ff990cca5b81886e0f33efdbd
Mark Furland <markfurland@gmail.com>
2016-07-10 19:38:51 -0400

    Comments! So useful.

--- arduinoKeypad.ino
+++ arduinoKeypad.ino
@@ -1,21 +1,19 @@
 #include <Keyboard.h>
 #include "Remote.h"
 
+//Set what pins you are using here.
+const int button_pins[] = {10,14,15,9,7,5};
 
-const int button_pins[] = {10,14,15,9,7,5};//Set what pins you are using here.
+//This is an array of function pointers. It relates to the button on the same pin in the button_pins array.
+void (*button_functions[])() = {&volDown, &volUp, &nothing, &hackADay, &boss, &pausePlay};
+
+//Debounce setup
 int button_state[] = {1,1,1,1,1,1};
 int button_state_last[] = {1,1,1,1,1,1};
 int debounce[] = {0,0,0,0,0,0};
 const int debounce_time = 50;
 
-void volUp();
-void volDown();
-void pausePlay();
-void nothing();
-
-//So this is an array of function pointers. This way, you can assign keys to functions easily.
-void (*button_functions[])() = {&volDown, &volUp, &nothing, &hackADay, &boss, &pausePlay};
-
+/**** Functions to run ****/
 void hackADay() {
   //Windows only, is example of keyboard stuff
   Keyboard.press(KEY_RIGHT_GUI);
@@ -56,8 +54,10 @@ void pausePlay() {
   Remote.clear();
 }
 
+/**** Setup and Loop ****/
 void setup() {
   int i = 0;
+  //Set each pin to be an input pin tied high
   for (; i < 6; ++i) {
     pinMode(button_pins[i], INPUT_PULLUP);
   }
@@ -65,12 +65,16 @@ void setup() {
 
 void loop() {
   int i = 0;
+  //Foreach pin run debouncing, and if it's a real press call the function
   for (; i < 6; ++i) {
-    button_state[i] = digitalRead(button_pins[i]);
+    button_state[i] = digitalRead(button_pins[i]);//Read current state
+    //If the current state does not equal the last state, AND it's been long enough since the last change
     if (button_state[i] != button_state_last[i] && millis() - debounce[i] > debounce_time) {
+      //If the button is pressed, call funciton
       if (button_state[i] == LOW) {
         button_functions[i]();
       }
+      //If the button was pressed or released, we still need to reset the debounce timer.
       button_state_last[i] = button_state[i];
       debounce[i] = millis();
     }
